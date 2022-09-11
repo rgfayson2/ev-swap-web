160014169a9f12bd8e7d8f967c219942e63a3d9d
Roderick Fayson <rgfayson2@gmail.com>
2022-09-10 22:14:11 -0400

    fixed carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Cars from "./pages/Cars"
+import Cars from "../src/pages/Cars"
 import Login from "../src/pages/Login"
 import SignUp from "../src/pages/SignUp"
 import AddCar from "../src/pages/AddCar"
--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,57 +1,48 @@
-import { useEffect } from "react"
-import { useState } from "react"
-import { Button, Carousel } from "react-bootstrap"
-import CarCard from "../components/CarCard";
-
+import Carousel from "react-bootstrap/Carousel"
+import CarCard from "../components/CarCard"
+import { useEffect,useState } from "react"
+import { Button } from "react-bootstrap"
 
 export default function Cars() {
-  const [cars, setCars] = useState()
-  // const [make, setMake] = useState()
-  // const [model, setModel] = useState()
-  // const [year, setYear] = useState()
-  // const [color, setColor] = useState()
+  const [cars, setCars] = useState([])
 
   const handleSelectedCar = (car) => {
-    // setStrategy([...car, currencypair]);
-    console.log(car);
-  };
-
-
-
+    console.log(car)
+  }
 
   useEffect(() => {
     fetch("http://localhost:4000/cars")
-      // fetch("https://https://ev-swap-api.web.app//cars",
+      // fetch("https://https://ev-swap-api.web.app//cars")
       .then((results) => results.json())
       .then((data) => setCars(data))
       .catch(console.error)
-  }, [])
+  }, []);
 
   return (
-    <div className="carousel-background"> 
-
-    <Carousel
-      className="container"
-      variant="light"
-      slide={false}
-      interval={null}
+    <div className="carousel-background">
+      <div></div>
+      <Carousel
+        className="container"
+        variant="light"
+        slide={false}
+        interval={null}
       >
-      {cars.map ((car,id) => (
-        <Carousel.Item key={id}>
-          <CarCard carObj={car} />
+        {cars.map((car, id) => (
+          <Carousel.Item key={id}>
+            <CarCard carObj={car} />
 
-          <Carousel.Caption>
-            <Button
-              className="cp-btn"
-              variant="outline-light"
-              onClick={(e) => handleSelectedCar(car)}
+            <Carousel.Caption>
+              <Button
+                className="cp-btn"
+                variant="outline-light"
+                onClick={() => handleSelectedCar(car)}
               >
-              Select Car
-            </Button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-      </div>
+                Select Car
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+       </Carousel>
+    </div>
   )
 }
